6df5360a7f014910319e54d71bb78597a0435b61
trav3ll3r <sinisa.poznic@gmail.com>
2018-02-06 17:52:57 +1100

    Simple chain reaction

---
+++ docs/mindmap.mm
@@ -0,0 +1,65 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Sign In" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1510025754173"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="SIGN_IN" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_466643035" CREATED="1510025322255" MODIFIED="1510025726574">
+<icon BUILTIN="positive"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="GetAccounts" LOCALIZED_STYLE_REF="default" ID="ID_1330398905" CREATED="1510025367076" MODIFIED="1510025653193"/>
+<node TEXT="GetCards" ID="ID_1160274168" CREATED="1510025374666" MODIFIED="1510025376546"/>
+<node TEXT="GetQuickBalance" ID="ID_1982599988" CREATED="1510025379424" MODIFIED="1510025388795"/>
+<node TEXT="GetNBA" ID="ID_1289175321" CREATED="1510025389658" MODIFIED="1510025391777"/>
+<node TEXT="ONBOARDING" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1463773317" CREATED="1510025442329" MODIFIED="1510025730773">
+<icon BUILTIN="positive"/>
+</node>
+</node>
+</node>
+</map>
